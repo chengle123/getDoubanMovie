6550fc1d52f74f6b4a543a5c8db3251dce70498d
chengle123 <314737853@qq.com>
2018-05-16 17:13:56 +0800

    1

--- models/config.js
+++ models/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const seq = new Sequelize('movie_sansantao','root','',{
-        host: '127.0.0.1',
+        host: 'localhost',
         port: 3306,
         dialect:'mysql',
         timezone: '+08:00',
